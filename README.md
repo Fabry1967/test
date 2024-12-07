7c8fbbc094b33835c5b8aba96903e68433f647af
Fabry1967 <sparviero46@hotmail.it>
2024-12-07 11:22:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Introduzione dei Signal in Angular 
+
+Questo progetto è stato generato con [Angular CLI](https://github.com/angular/angular-cli) versione 16.0.1.
+
+## Server di sviluppo
+
+Esegui `ng serve` per un server di sviluppo. Poi puoi andare su `http://localhost:4200/`. L'applicazione si ricaricherà automaticamente se viene modificato qualsiasi tipo di file.
+
+## Articolo
+
+Una nuova entusiasmante funzionalità è arrivata su Angular: i Segnali (Signal). Forniscono un nuovo modo di comunicare ai nostri template che i dati sono stati cambiati. Ciò migliora il rilevamento delle modifiche, aumenta le prestazioni e rende il nostro codice più reattivo.
+
+Puoi trovare il codice di “Angular Signals Intro” su https://github.com/Impesud/angular-signals-intro e https://stackblitz.com/edit/angular-signals-intro
+
+Si parlerà di alcuni concetti fondamentali per i segnali: change detection, computed signal, consumers, getter, signal components, consumer interest, RxJs, observables, service.  
+
+L'articolo completo lo trovi su https://www.impesud.it/introduzione-dei-signal-in-angular/
+
+
---
+++ src/main.ts
@@ -0,0 +1,102 @@
+/*
+Introduction to using Signals in Angular 16
+Follow us: https://github.com/Impesud
+*/
+import 'zone.js/dist/zone';
+import {
+  Component,
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+/*[style.color]="color()">Shopping Cart</h1>*/
+@Component({
+  selector: 'pippo',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  template: `
+  
+    <h1  class="box-3">Shopiiiii</h1>
+    <div class="box-3">Ship:{{minnie()}} </div>
+    `
+  
+})
+export class Miopippo {
+  minnie= signal<number>(1);
+}
+@Component({
+  selector: 'angular-signals-app',
+  standalone: true,
+  imports: [CommonModule,FormsModule,Miopippo],
+  template: `
+      <h1  class="box-2">Shopping Cart</h1>
+      <select
+        [ngModel]="quantity()"
+        (change)="onQuantitySelected($any($event.target).value)">
+        <option disabled value="">--Select a quantity--</option>
+        <option *ngFor="let q of qtyAvailable()">{{ q }}</option>
+      </select>
+      <div style="background: red">Ship: {{ selectedShip().name}}</div>
+      <div>Price: {{ selectedShip().price | number: '1.2-2'}}</div>
+      <div style="font-weight: bold" [style.color]="color()">Total: {{ totalPrice()  | number: '1.2-2' }}</div>
+      <div *ngFor="let v of ships()">List of ships: {{ v.name }}</div>
+      <pippo>Loading...</pippo>`
+  
+})
+
+export class App {
+  quantity = signal<number>(1);
+  qtyAvailable = signal([1, 2, 3, 4, 5, 6]);
+
+  selectedShip = signal<Ship>({
+     id: 1, name: 'Nazca', price: 10000 
+  });
+
+  ships = signal<Ship[]>([]);
+
+  totalPrice = computed(() => this.selectedShip().price * this.quantity());
+  color = computed(() => this.totalPrice() > 50000 ? 'green' : 'blue');
+
+  constructor() {
+    
+    //Print the initial quantity
+    console.log(this.quantity());
+
+    // Default quantity
+    this.quantity.update((qty) => qty * 3);
+
+    // Interstellar price increase
+    this.selectedShip.mutate((v) => v.price = v.price + (v.price * 0.2));
+
+    // Add selected ship to array
+    this.ships.mutate(v => v.push(this.selectedShip()))
+
+    // Example of an effect
+    effect(() => console.log(JSON.stringify(this.ships())));
+  }
+
+  // Example of a declarative effect
+  qtyEff = effect(() => console.log("Latest quantity:", this.quantity()));
+
+  onQuantitySelected(qty: number) {
+    this.quantity.set(qty);
+
+    // Does not "emit" values, rather updates the value in the "box"
+    // this.quantity.set(5);
+    // this.quantity.set(42);
+
+    // Add the ship to the array again ... to see the effect execute
+    //this.ships.mutate(v => v.push(this.selectedShip()))
+  }
+}
+
+export interface Ship {
+  id: number;
+  name: string;
+  price: number;
+}
+
+bootstrapApplication(App);
